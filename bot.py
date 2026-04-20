56c067740efee5934586a31ed41b0beaa6b82510
cybermozilla <cybermozilla@gmail.com>
2026-04-20 11:56:34 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ TOKEN_BOT = "7864266536:AAHW3BMrhRqDzeUsaydQQ6Pum5KbCpG8GEM"
 API_KEY_TIENDA = "2fc6bc5920314acce1467adb2e95dbd369e7312f31a7c465f6aef0fb86d7537d"
 URL_TELEGRAM = f"https://api.telegram.org/bot{TOKEN_BOT}"
 URL_TIENDA = "https://tiendagiftven.tech/api/v1"
-DATABASE_URL = "postgresql://postgres:(BDMICRO88)@db.djicdsioescrhoydlvdz.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres:BDMICRO88ec@db.djicdsioescrhoydlvdz.supabase.co:5432/postgres"
 
 app = Flask(__name__)
 sesiones = {}
